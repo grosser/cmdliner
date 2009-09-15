def38eb7a76dddbaf540cd8b85110638bb804d39
grosser <grosser.michael@gmail.com>
2009-09-15 08:31:22 +0200

    man parser refactor

--- lib/man_parser.rb
+++ lib/man_parser.rb
@@ -1,20 +1,20 @@
 class ManParser
   def self.parse(cmd)
     text = `man #{cmd}`
-    description, options = parse_description(text)
-    {:description => description.join(''), :options=>options}
+    sections = sections(text)
+    puts sections.inspect
+    description, options = parse_description(sections['DESCRIPTION'])
+    {:description => description.join(''), :options=>options, :sections=>sections}
   end
 
   private
 
   def self.parse_description(text)
-    section = section('DESCRIPTION', text)
-    
     in_option = false
     options = []
     description = []
 
-    section[1..-1].each do |line|
+    text[1..-1].each do |line|
 
       if start_of_option?(line)
         in_option = true
@@ -36,13 +36,16 @@ class ManParser
     [description, options]
   end
 
-  def self.section(name, text)
-    in_section = false
-    text.split("\n").map do |line|
-      in_section = false if line =~ /^[A-Z]+$/
-      in_section = true if line == name
-      in_section ? line : nil
-    end.compact
+  def self.sections(text)
+    name = 'OUT_OF_SECTION'
+    sections = Hash.new([])
+
+    text.split("\n").each do |line|
+      name = line if line =~ /^[A-Z]+$/
+      sections[name] += [line]
+    end
+
+    sections
   end
 
   def self.start_of_option?(line)
