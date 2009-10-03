2d9b530854e81766c4aa6da3b24069f76a2a9cfe
grosser <grosser.michael@gmail.com>
2009-10-03 17:48:46 +0200

    first successful generates_command run

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.gem "grosser-rpx_now", :lib => 'rpx_now', :version => '0.5.9', :source=>"http://gems.github.com"
-  config.gem "man_parser", :version => '0.1.1', :source=>"http://gemcutter.org"
+  config.gem "man_parser", :version => '0.1.3', :source=>"http://gemcutter.org"
 
   config.time_zone = 'UTC'
 
--- lib/tasks/cl.rake
+++ lib/tasks/cl.rake
@@ -3,7 +3,9 @@ namespace :cl do
   task :generate_commands  => :environment do
     ManParser.available_commands.each do |cmd|
       next if Command.find_by_name(cmd)
+      puts "now #{cmd}"
       data = ManParser.parse(cmd)
+      next if data.empty?
       cmd = Command.new(:name=>cmd, :description => data[:description])
       data[:options].each do |option|
         cmd.options.build(option)
